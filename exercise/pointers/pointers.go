06bdfedbc433716f17bce4eb176341ede60f70c6
david <david@davids-MacBook-Pro.local>
2023-09-01 18:21:53 +0300

    added pointers solution

--- exercise/pointers/pointers.go
+++ exercise/pointers/pointers.go
@@ -18,6 +18,57 @@ package main
 
 import "fmt"
 
-func main() {
+// * Create a structure to store items and their security tag state
+//   - Security tags have two states: active (true) and inactive (false)
+const (
+	Active   = true
+	Inactive = false
+)
 
+type Tag bool
+
+type Item struct {
+	name string
+	tag  Tag
+}
+
+// * Create functions to activate and deactivate security tags using pointers
+func activate(tag *Tag) {
+	*tag = Active
+}
+func deactivate(tag *Tag) {
+	*tag = Inactive
+}
+
+// * Create a checkout() function which can deactivate all tags in a slice
+func checkout(items []Item) {
+	fmt.Println("Checking out items...")
+	for i := 0; i < len(items); i++ {
+		deactivate(&items[i].tag)
+	}
+}
+
+func main() {
+	//* Perform the following:
+	//  - Create at least 4 items, all with active security tags
+	shirt := Item{name: "shirt", tag: Inactive}
+	pants := Item{name: "pants", tag: Inactive}
+	cap := Item{name: "cap", tag: Inactive}
+	jacket := Item{name: "jacket", tag: Inactive}
+	// fmt.Println(shirt)
+	activate(&shirt.tag)
+	activate(&cap.tag)
+	activate(&jacket.tag)
+
+	//  - Store them in a slice or array
+	items := []Item{shirt, pants, cap, jacket}
+	fmt.Println(items)
+
+	//  - Deactivate any one security tag in the array/slice
+	deactivate(&items[2].tag)
+	fmt.Println(items)
+
+	//  - Call the checkout() function to deactivate all tags
+	checkout(items)
+	fmt.Println(items)
 }
